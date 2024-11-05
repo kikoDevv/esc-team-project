557c28d70184564eb1534ecfcea2840a9fd4feba
Christian Fryksten <christianfryksten1@gmail.com>
2024-11-05 13:05:14 +0100

    JS Eventhandler logic

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const navButton = document.querySelector(".nav-button");
+const hamburgerButton = document.querySelector(".hamburger-button");
+const headerNav = document.querySelector(".header-nav");
 
-navButton.addEventListener("click", () => {
+hamburgerButton.addEventListener("click", () => {
   console.log("Nav Button was pressed");
-  const headerNav = document.querySelector(".header-nav");
+  const exitNavButton = document.createElement("button");
+  const exitNavButtonImg = document.createElement("img");
+  headerNav.appendChild(exitNavButton);
+  exitNavButton.appendChild(exitNavButtonImg);
+  exitNavButtonImg.setAttribute("src", "./pictures/nav-button-close.png");
+  exitNavButton.classList.add("exitNavButton");
   headerNav.style.display = "flex";
 });
+
+/* const allElements = document.querySelectorAll("*");
+
+allElements.forEach((element) => {
+  if (element !== headerNav) {
+    element.style.opacity = "0.5";
+  }
+});
+ */
