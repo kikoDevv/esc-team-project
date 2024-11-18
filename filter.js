862b2a03c2ab7148f8480b95eea398e42039654a
0x0xoscar <139160108+0x0xoscar@users.noreply.github.com.>
2024-11-18 21:55:55 +0100

    Added some of the filter functions

---
+++ filter.js
@@ -0,0 +1,71 @@
+const includeOnline = document.querySelector("#includeOnline");
+const includeOnsite = document.querySelector("#includeOnsite");
+const filterInput = document.querySelector(".filterInput");
+
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://lernia-sjj-assignments.vercel.app/api/challenges");
+
+        if (!response.ok) {
+            throw new Error(`HTTP ERROR! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getChallenges() {
+    const challengesArray = await fetchData();
+    console.log(challengesArray)
+}
+
+
+getChallenges();
+
+
+async function sortByType() {
+    const response = await fetchData();
+    const challengesArray = response.challenges;
+
+    const onlineArray = challengesArray.filter(challenge => challenge.type === 'online');
+    const onsiteArray = challengesArray.filter(challenge => challenge.type === 'onsite');
+
+    if ((includeOnline.checked && includeOnsite.checked) || (!includeOnline.checked && !includeOnsite.checked)) {
+        createCardsFromAPI(challengesArray);
+        console.log(challengesArray)
+    } else {
+        if (includeOnline.checked) {
+            createCardsFromAPI(onlineArray);
+            console.log(onlineArray)
+        }
+
+        if (includeOnsite.checked) {
+            createCardsFromAPI(onsiteArray);
+            console.log(onsiteArray)
+        }
+    }
+}
+
+async function sortByText() {
+    const response = await fetchData();
+    const challengesArray = response.challenges;
+
+    const input = filterInput.value.toLowerCase();
+
+    const matchedChallenges = challengesArray.filter(challenge => challenge.title.toLowerCase().includes(input) || challenge.description.toLowerCase().includes(input))
+
+
+    if (matchedChallenges.length > 0) {
+        createCardsFromAPI(matchedChallenges);
+    }
+
+}
+
+includeOnline.addEventListener("change", sortByType)
+includeOnsite.addEventListener("change", sortByType)
+filterInput.addEventListener("keydown", sortByText)
