116d8ec3b645fdda03edaf89ed6e31d58147c590
Christian Fryksten <christianfryksten1@gmail.com>
2024-11-20 14:41:35 +0100

    Add functionality to chose dates, check dates and post to API

--- booking.js
+++ booking.js
@@ -1,3 +1,24 @@
+async function fetchBookingTimes(date, id) {
+  try {
+    const response = await fetch(
+      "https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=" +
+        date +
+        "&challenge=" +
+        id
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP ERROR! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    data.slots.forEach((slot) => {
+      console.log(slot);
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
@@ -29,39 +50,31 @@ function openBookingPageOne(ID, minParticipants, maxParticipants) {
       console.log(chosenDate);
       document.querySelector(".book-page-one").remove();
 
-      openBookingPageTwo(ID, minParticipants, maxParticipants);
+      openBookingPageTwo(ID, minParticipants, maxParticipants, chosenDate);
     }
   });
 }
 
 // Function to generate booking page two
-function openBookingPageTwo(ID, minParticipants, maxParticipants) {
+async function openBookingPageTwo(ID, minParticipants, maxParticipants, date) {
   const section = document.createElement("section");
   section.className = "booking-step-two";
   section.innerHTML = `
        <p>Work is ongoing..!!<p>
        <p>book room title of room step 2</p>
        <p>name</p>
-       <input class="input-name" type="text">
+       <input class="input-name" type="text" required>
        <p>E-mail</p>
-       <input class="input-email" type="email">
-       <p>What time</p>
+       <input class="input-email" type="email" required>
        <form>
-           <label for="start-time">from:</label>
-           <input type="time" id="start-time" name="start-time" required>
-       
-           <label for="end-time">to:</label>
-           <input type="time" id="end-time" name="end-time" required>
+           <label for="what-time">What time?</label>
+           <select id="what-time" name="time" required>
+           </select>
        </form>
        <form>
            <label for="participants-count">How many participants:</label>
-           <select id="participants-count" name="options">
-  <!--         <option value="1">1 participant</option>
-               <option value="2">2 participants</option>
-               <option value="3">3 participants</option>
-               <option value="4">4 participants</option>
-               <option value="2">5 participants</option>
-               <option value="1">6 participants</option>  -->
+           <select id="participants-count" name="options ">
+  <!--         <option value="1">1 participant</option>  -->
            </select>
        </form>
        <button class="submit-booking">Submit booking</button>
@@ -69,6 +82,7 @@ function openBookingPageTwo(ID, minParticipants, maxParticipants) {
   document.body.appendChild(section);
   const submitBtn = document.querySelector(".submit-booking");
 
+  /* Generate options for participants */
   for (let i = minParticipants; i <= maxParticipants; i++) {
     const option = document.createElement("option");
     option.setAttribute("value", `${i}`);
@@ -76,17 +90,54 @@ function openBookingPageTwo(ID, minParticipants, maxParticipants) {
     document.querySelector("#participants-count").prepend(option);
   }
 
-  /* max - min + 1 */
-  /* 5 - 2 + 1 */
+  const data = await fetchBookingTimes(date, ID);
+
+  /* Generate options for times */
+  for (let i = 0; i < data.slots.length; i++) {
+    const option = document.createElement("option");
+    option.setAttribute("value", data.slots[i]);
+    option.innerText = data.slots[i];
+    document.querySelector("#what-time").prepend(option);
+  }
+
+  console.log(data.slots);
 
   submitBtn.addEventListener("click", () => {
     const bookedName = document.querySelector(".input-name").value;
     const bookedEmail = document.querySelector(".input-email").value;
+    const chosenTime = document.querySelector("#what-time").value;
+    const chosenParticipants = parseInt(
+      document.querySelector("#participants-count").value
+    );
+
+    const url =
+      "https://lernia-sjj-assignments.vercel.app/api/booking/reservations";
+
+    const data = {
+      challenge: ID,
+      name: bookedName,
+      email: bookedEmail,
+      date: date,
+      time: chosenTime,
+      participants: chosenParticipants,
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((result) => console.log("Success: ", result))
+      .catch((error) => console.error("Error: ", error));
 
     document.querySelector(".booking-step-two").remove();
   });
 }
-
-bookRoomBtn.addEventListener("click", () => {
-  openBookingPageOne();
-});
