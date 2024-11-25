e00a6125807f394758c18cff8797ea4d57639a80
Christian Fryksten <christianfryksten1@gmail.com>
2024-11-25 18:47:54 +0100

    Add first Cypress test to try booking a challenge

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe("first test", () => {
+describe("Booking test", () => {
   it("Enters the home page", () => {
     cy.visit("index.html");
+    cy.get("button").contains("On-site challenges").click();
+    cy.get("button").contains("Take challenge online").click();
+    cy.get(".date-input").click().type("2024-12-24");
+    cy.get("#btn-search-time").click();
+    cy.get("#input-name").type("Robot!");
+    cy.get("#input-email").type("1337@gmail.com");
+    cy.get(".submit-booking").click();
   });
 });
